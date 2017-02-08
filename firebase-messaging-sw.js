7e86900597d0eb7db3c4e7f8fe7301d5fbd1cb74
Peter Gribanov <wcode404@gmail.com>
2017-02-08 14:54:05 +0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,8 @@
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+
+firebase.initializeApp({
+    'messagingSenderId': '448358493027'
+});
+
+const messaging = firebase.messaging();
