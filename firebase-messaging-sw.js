81dde816d2dbb13e6b1160cbce36aee37d15da1a
Peter Gribanov <wcode404@gmail.com>
2017-02-09 16:31:12 +0300

    try show message

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,7 +2,11 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '448358493027'
+    messagingSenderId: '448358493027'
 });
 
-firebase.messaging();
+const messaging = firebase.messaging();
+
+messaging.onMessage(function(payload) {
+    console.log('Message received. ', payload);
+});
