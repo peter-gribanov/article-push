7462c9236e0f2a9a66ae3184357b74e89ce4c6d7
Peter Gribanov <wcode404@gmail.com>
2017-02-09 16:46:15 +0300

    use firebase of version 3.6.8

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,12 +1,8 @@
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
 
 firebase.initializeApp({
     messagingSenderId: '448358493027'
 });
 
 const messaging = firebase.messaging();
-
-messaging.onMessage(function(payload) {
-    console.log('Message received. ', payload);
-});
