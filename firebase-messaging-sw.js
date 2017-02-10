ab45430c40831f3204b61136c5d9379429de3844
Peter Gribanov <wcode404@gmail.com>
2017-02-10 12:50:03 +0300

    restore customize notification handler

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -6,3 +6,32 @@ firebase.initializeApp({
 });
 
 const messaging = firebase.messaging();
+
+// Customize notification handler
+messaging.setBackgroundMessageHandler(function(payload) {
+    payload.data.data = payload.data;
+
+    return self.registration.showNotification(payload.data.title, payload.data);
+});
+
+self.addEventListener('notificationclick', function(event) {
+    var target = event.notification.data.click_action || '/';
+    event.notification.close();
+
+    // This looks to see if the current is already open and focuses if it is
+    event.waitUntil(clients.matchAll({
+        type: 'window'
+    }).then(function(clientList) {
+        // clientList always is empty?!
+        for (var i = 0; i < clientList.length; i++) {
+            var client = clientList[i];
+            if (client.url == target && 'focus' in client) {
+                return client.focus();
+            }
+        }
+
+        if (clients.openWindow) {
+            return clients.openWindow(target);
+        }
+    }));
+});
